6c79bb17516a20cffb2277122f2700d0651c96b9
pavithrani <pererapavithrani02@gmail.com>
2024-04-30 19:18:51 +0530

    update javascript

--- assets/Javascript/script.js
+++ assets/Javascript/script.js
@@ -4,80 +4,77 @@
 }
     var css2 ={
     display: "block",
-    paddingBottom: "20px"
+
 }
-    $("#homeSection").css(css2);
-    $("#customerSection").css(css1);
-    $("#addedItemSection").css(css1);
-    $("#itemSection").css(css1);
-    $("#profileSection").css(css1);
-    $("#ReportSection").css(css1);
-    $("#orderSection").css(css1);
 
 
-    $("#nav-item").click(function (){
-    $("#homeSection").css(css1);
-    $("#customerSection").css(css1);
-    $("#addedItemSection").css(css1);
-    $("#itemSection").css(css2);
-    $("#profileSection").css(css1);
-    $("#ReportSection").css(css1);
-    $("#orderSection").css(css1);
-});
-    $("#nav-home").click(function (){
-    $("#homeSection").css(css2);
-    $("#customerSection").css(css1);
-    $("#addedItemSection").css(css1);
-    $("#itemSection").css(css1);
-    $("#profileSection").css(css1);
-    $("#ReportSection").css(css1);
-    $("#orderSection").css(css1);
-});
+   function handleNavClick(clickedElementId){
 
-    $("#nav-customer").click(function (){
-    $("#homeSection").css(css1);
-    $("#customerSection").css(css2);
-    $("#addedItemSection").css(css1);
-    $("#itemSection").css(css1);
-    $("#profileSection").css(css1);
-    $("#ReportSection").css(css1);
-    $("#orderSection").css(css1);
-});
+       switch (clickedElementId) {
+           case "nav-home":
+               showSection("homeSection");
+               break;
+           case "nav-customer":
+               showSection("customerSection");
+               break;
+           case "nav-item":
+               showSection("itemSection");
+               break;
 
-    $("#nav-order").click(function (){
-    $("#homeSection").css(css1);
-    $("#customerSection").css(css1);
-    $("#addedItemSection").css(css1);
-    $("#itemSection").css(css1);
-    $("#profileSection").css(css1);
-    $("#ReportSection").css(css1);
-    $("#orderSection").css(css2);
-});
+           case "nav-order":
+               showSection("orderSection");
+               break;
 
-    $("#nav-profile").click(function (){
-    $("#homeSection").css(css1);
-    $("#customerSection").css(css1);
-    $("#addedItemSection").css(css1);
-    $("#itemSection").css(css1);
-    $("#profileSection").css(css2);
-    $("#ReportSection").css(css1);
-    $("#orderSection").css(css1);
-});
-    $("#addedItem").click(function (){
-    $("#homeSection").css(css1);
-    $("#customerSection").css(css1);
-    $("#addedItemSection").css(css2);
-    $("#itemSection").css(css1);
-    $("#profileSection").css(css1);
-    $("#ReportSection").css(css1);
-    $("#orderSection").css(css1);
-});
-    $("#nav-report").click(function (){
-    $("#homeSection").css(css1);
-    $("#customerSection").css(css1);
-    $("#addedItemSection").css(css1);
-    $("#itemSection").css(css1);
-    $("#profileSection").css(css1);
-    $("#ReportSection").css(css2);
-    $("#orderSection").css(css1);
-});
+           case "nav-profile":
+               showSection("profileSection");
+               break;
+
+           case "addedItem":
+               showSection("addedItemSection");
+               break;
+
+           case "nav-report":
+               showSection("ReportSection");
+
+       }
+   }
+   function showSection(sectionId){
+       $("#homeSection").css(css1);
+       $("#customerSection").css(css1);
+       $("#addedItemSection").css(css1);
+       $("#itemSection").css(css1);
+       $("#profileSection").css(css1);
+       $("#ReportSection").css(css1);
+       $("#orderSection").css(css1);
+       $(`#${sectionId}`).css(css2);
+   }
+
+    $("#nav-item").click(function () {
+        handleNavClick($(this).attr("id"));
+    });
+
+    $("#nav-home").click(function () {
+        handleNavClick($(this).attr("id"));
+    });
+    $("#nav-report").click(function () {
+        handleNavClick($(this).attr("id"));
+    });
+    $("#nav-profile").click(function () {
+        handleNavClick($(this).attr("id"));
+    });
+    $("#nav-order").click(function () {
+        handleNavClick($(this).attr("id"));
+    });
+    $("#nav-customer").click(function () {
+        handleNavClick($(this).attr("id"));
+    });
+
+    $("#addedItem").click(function () {
+        handleNavClick($(this).attr("id"));
+    });
+    $(document).ready(function() { // This function runs when the document is ready
+        handleNavClick("nav-home");
+    });
+
+
+
