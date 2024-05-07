ed8b59975c4a905c2a18ea6f2a0b8d97435d0bfc
pavithrani <pererapavithrani02@gmail.com>
2024-05-07 18:56:05 +0530

    create AddedItemModal.js

---
+++ model/AddedItemModal.js
@@ -0,0 +1,51 @@
+ export  default class AddedItemModal{
+
+    constructor(name ,price ,quantity,category,description) {
+        this._name = name;
+        this._price =price;
+        this._quantity = quantity;
+        this._category  = category;
+        this._description = description;
+
+    }
+
+    get name() {
+        return this._name;
+    }
+
+    set name(value) {
+        this._name = value;
+    }
+
+    get price() {
+        return this._price;
+    }
+
+    set price(value) {
+        this._price = value;
+    }
+
+    get quantity() {
+        return this._quantity;
+    }
+
+    set quantity(value) {
+        this._quantity = value;
+    }
+
+    get category() {
+        return this._category;
+    }
+
+    set category(value) {
+        this._category = value;
+    }
+
+    get description() {
+        return this._description;
+    }
+
+    set description(value) {
+        this._description = value;
+    }
+}
