78bd297e70ecc43d96f3f94fd17108677838f8f1
pavithrani <pererapavithrani02@gmail.com>
2024-05-08 07:11:17 +0530

    dynamically added item card

--- assets/Javascript/Item.js
+++ assets/Javascript/Item.js
@@ -1,25 +1,24 @@
 $(document).ready(function (){
-    $(".item-button").click( function (){
-        console.log("clicked")
-        var itemName =$(this).closest(".card-body").find(".card-title").text();
-        var itemImageSrc =$(this).closest(".item-card").find(".card-img-top").attr("src");
+    $(document).on("click", ".item-button", function () {
+        console.log("clicked");
+        var itemName = $(this).closest(".card-body").find(".card-title").text();
+        var itemImageSrc = $(this).closest(".item-card").find(".card-img-top").attr("src");
         var itemWeight = $(this).closest(".item-card").find(".form-select").val();
         var itemPriceText = $(this).closest(".item-card").find(".card-text").text();
         var itemPrice = parseFloat(itemPriceText.replace("Price: Rs.", ""));
 
         var newItem = `
-    <div class="cart-item">
-        <img src="${itemImageSrc}" alt="Item Image" class="item-image">
-        <span class="item-name">${itemName}</span>
-        <input type="number" class="form-control quantity-input" value="1" min="1">
-        <span class="item-price">Rs. ${itemPrice.toFixed(2)}</span>
-    </div>
-`;
+        <div class="cart-item">
+            <img src="${itemImageSrc}" alt="Item Image" class="item-image">
+            <span class="item-name">${itemName}</span>
+            <input type="number" class="form-control quantity-input" value="1" min="1">
+            <span class="item-price">Rs. ${itemPrice.toFixed(2)}</span>
+        </div>
+    `;
         $(".shopping-cart").append(newItem);
 
         updateTotalPrice();
-
-    })
+    });
 
     function updateTotalPrice(){
         var totalPrice =0;
